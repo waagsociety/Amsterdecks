3fc5a766c230de5739b8af09d34c39746132024c
Martin Risseeuw <hello@martinr.nl>
2015-07-20 14:31:54 +0200

    updated styles

--- Speeltuin/public/js/controls.js
+++ Speeltuin/public/js/controls.js
@@ -1,23 +1,32 @@
 var sideBarWidth = document.getElementById('sideBar').offsetWidth;
 var trailLenght = 0.75;
+
 var controls = function() {
   
 };
 
 function initMotionDisplayControls(motionDisplay) {
-  var gui = new dat.GUI({ autoPlace: false, width: sideBar });
   var control = new controls();
+  var gui = new dat.GUI({ autoPlace: false, width: sideBarWidth - 1, load: JSON });
+
+  
+    
+  gui.remember(control);
+
   gui.add(motionDisplay, 'timeStep').min(0).max(0.1).onChange(function(val){
     motionDisplay.grid.timePassing = true;
     return val;
   });
-  gui.add(motionDisplay.grid, 'T').min(0).max(motionDisplay.grid.variant.length).onChange(function(newValue) {
+
+  gui.add(motionDisplay.grid, 'T').min(0).max(motionDisplay.grid.variant.length).listen().onChange(function(newValue) {
     motionDisplay.grid.timePassing = false;
     motionDisplay.grid.setT(newValue);
   });
+
   gui.add(defaults.motionDisplay, 'particleSpdFactor').min(0.02).max(.9).onChange(function(newValue) {
     console.log("Value changed to:  ", newValue);
   });
+  
   gui.add(window, 'trailLenght').min(0.5).max(0.95).onChange(function(newValue) {
     var background = motionDisplay.background,
         split = background.split(',');
@@ -38,16 +47,27 @@ function initVariationControls(motionDisplay){
   var wetBtn = document.getElementById('wetButton'), 
       dryBtn = document.getElementById('dryButton'),
       avgBtn = document.getElementById('avgButton');
+  
   wetBtn.onclick = function() {
     this.classList.add("active");
-    motionDisplay.grid.nextVariant();
+    dryBtn.classList.remove('active');
+    avgBtn.classList.remove('active');
+    motionDisplay.grid.nextVariant('wet');
   },
   dryBtn.onclick = function() {
     this.classList.add("active");
-    motionDisplay.grid.nextVariant();
+    wetBtn.classList.remove('active');
+    avgBtn.classList.remove('active');
+    motionDisplay.grid.nextVariant('dry');
   },
   avgBtn.onclick = function() {
     this.classList.add("active");
-    motionDisplay.grid.nextVariant();
+    wetBtn.classList.remove('active');
+    dryBtn.classList.remove('active');
+    motionDisplay.grid.nextVariant('avg');
   }
 }
+
+//function perspectiveView(motionDisplay) {
+//    motionDisplay.canvas.style.transform = "rotateX(5deg) translateY( -100% ) scale(1.1)"; 
+//}
