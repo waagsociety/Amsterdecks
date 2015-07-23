73290153e55171c9fb6ef75dfe25ae3bbbbc28f9
Bert Spaan <bert.spaan@gmail.com>
2015-07-23 12:13:46 +0200

    relative paths!!!!

--- public/js/init.js
+++ public/js/init.js
@@ -5,12 +5,12 @@ function init(){
 	Object.keys(fieldSets).forEach(function(name){
     var sideControlContainer = document.getElementById('sideContainer');
     var element = document.createElement('a');
-    
+
     element.innerHTML = name;
     element.href = "#" + name + "" ;
     element.classList.add('btn', 'btn-1','btn-1a');
     element.setAttribute('data-setname', name);
-    
+
     element.onclick = function(e){
       e.preventDefault();
 			loadField(fieldSets[this.dataset.setname]);
@@ -21,11 +21,11 @@ function init(){
 }
 
 function loadField(options){
-	  
+
   var mapElement = document.getElementById('map');
-  
-  options.path = '/public/fields/';
-  
+
+  options.path = 'public/fields/';
+
   fieldsLoader(options, function(err, fields){
     console.log(err || 'starting field system');
     if(err) throw(err);
@@ -33,7 +33,7 @@ function loadField(options){
     //document.querySelector('section').appendChild(motionDisplay.canvas);
     var dynamicGridOptions = {
       width: $(mapElement).innerWidth(),
-      height: $(mapElement).innerHeight(),	
+      height: $(mapElement).innerHeight(),
     },
     importedGridOptions = {
       width: options.x,//1107,//336,
@@ -41,12 +41,12 @@ function loadField(options){
       fields: fields,
       timePassing: true
     };
-    
+
     var winAspect = $(mapElement).innerWidth() / $(mapElement).innerHeight(),
 			fieldAspect = importedGridOptions.width / importedGridOptions.height,
 			wider = fieldAspect > winAspect,
 			scale = !wider ? $(mapElement).innerWidth() / importedGridOptions.width : $(mapElement).innerHeight() / importedGridOptions.height;
-    
+
     var motionDisplay = new MotionDisplay({
       debugField: false,
       background: 'rgba(11,41,91,'+ trailLenght +')',
@@ -56,16 +56,16 @@ function loadField(options){
 			clipPath: clipPaths[options.setName]
     });
     window.md = motionDisplay;
-    
+
     initMotionDisplayControls(motionDisplay);
     initVariationControls(motionDisplay);
-    
+
     mapElement.appendChild(motionDisplay.canvas);
-    
+
     motionDisplay.canvas.style.width = Math.floor(importedGridOptions.width * scale) + 'px';
 		motionDisplay.canvas.style.height = Math.floor(importedGridOptions.height * scale) + 'px';
-    
-    
+
+
     var fieldButton = document.getElementById('fieldBtn');
     fieldButton.onclick=function(){
         md.debugField = !md.debugField;
